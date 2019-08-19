dc9540aaea32c42e77aa752fad8a5e037d24583a
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-08-19 13:19:48 -0500

    put columns within a Section so that they are aligned with the Hero

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React from 'react';
 //import logo from './logo.svg';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './App.scss';
-import { Columns, Container } from 'react-bulma-components';
+import { Columns, Container, Section, Hero, Heading } from 'react-bulma-components';
 
 function App() {
   return (
     <div className="QuinielaUI">
       <div className="App-body">
         <Container>
-          <Columns>
-            <Columns.Column size="one-fifth">20% width of container</Columns.Column>
-            <Columns.Column>80% width of container</Columns.Column>
-          </Columns>
+          <Section>
+            <Hero color="primary">
+              <Hero.Body>
+                <Container>
+                  <Heading>
+                    Quiniela Acumulativa Liga MX
+                  </Heading>
+                  <Heading subtitle size={3}>
+                    Jornada 5
+                  </Heading>
+                </Container>
+              </Hero.Body>
+            </Hero>
+          </Section>
+          <Section>
+            <Columns>
+              <Columns.Column size="one-third">33.33% width of container</Columns.Column>
+              <Columns.Column size="one-third">33.33% width of container</Columns.Column>
+              <Columns.Column size="one-third">33.33% width of container</Columns.Column>
+            </Columns>
+          </Section>
         </Container>
       </div>
     </div>
