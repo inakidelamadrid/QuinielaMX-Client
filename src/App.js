53fb8c481414a78f41c1692ffaf08d4fcb8a61a8
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-08-19 12:27:04 -0500

    Start by installing react bulma components and the initial grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 //import logo from './logo.svg';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './App.scss';
+import { Columns, Container } from 'react-bulma-components';
 
 function App() {
   return (
     <div className="QuinielaUI">
       <div className="App-body">
-        <div className="Box-hero">
-          Content
-        </div>
+        <Container>
+          <Columns>
+            <Columns.Column size="one-fifth">20% width of container</Columns.Column>
+            <Columns.Column>80% width of container</Columns.Column>
+          </Columns>
+        </Container>
       </div>
     </div>
   );
